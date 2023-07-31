00f826a21313ad405c26a3c5ba0379dc1002df85
billera <lukasbillera01@gmail.com>
2023-08-01 00:38:37 +0200

    clean

--- src/rotational_utils.jl
+++ src/rotational_utils.jl
@@ -38,14 +38,14 @@ Typ = Float32
 
 function get_rotation(M, N)
     """
-    Gets M x N random rotation matrices formatted as an array of size 3x3xN. 
+    Gets M x N random rotation matrices formatted as an array of size 3x3xMxN. 
     """
     return Typ.(reshape(rotmatrix_from_quat(bcds2flatquats(randn(3,M*N))),3,3,M,N))
 end
 function get_rotation(N) return Typ.(reshape(rotmatrix_from_quat(bcds2flatquats(randn(3,N))),3,3,N)) end
 function get_translation(M, N)
     """
-    Gets M x N random translations formatted as an array of size 3x1xNxM.
+    Gets M x N random translations formatted as an array of size 3x1xMxN.
     """
     return Typ.(randn(3,1, M, N))
 end
@@ -54,40 +54,29 @@ function get_translation(N) return Typ.(randn(3,1,N)) end
 
 function T_R3(mat, rot,trans)
     """ 
-    Applies the SE3 transformations T = (rot,trans) ∈ SE(E3)^N
-    to N batches of m points in R3, i.e., mat ∈ R^(3 x m x N) ↦ T(mat) ∈ R^(3 x m x N).
-    Note here that rotations here are represented in matrix form. 
+    Applies the SE3 transformations T = (rot,trans) ∈ SE(3)^N to m batches of N points in R3, i.e., mat ∈ R^(3 x N x m) ↦ T(mat) ∈ R^(3 x N x m).
     """
     size_mat = size(mat)
     rotc = reshape(rot, 3,3,:)  
     trans = reshape(trans, 3,1,:)
     matc = reshape(mat,3,size(mat,2),:)
-    @show size(rotc), size(matc) 
     batched_mul(rotc, matc)
-    if trans != 0
-        rotated_mat = batched_mul(rotc,matc) .+ trans
-    else 
-        rotated_mat = batched_mul(rotc,matc)
-    end
+    rotated_mat = batched_mul(rotc,matc) .+ trans
+    
     return reshape(rotated_mat,size_mat)
 end 
 
 function T_R3_inv(mat,rot,trans)
     """ 
-    Applys the group inverse of the SE3 transformations T = (rot,trans) ∈ SE(3)^N to N batches of m points in R3,
-    i.e., mat ∈ R^(3 x m x N) ↦ T^(-1)(mat) ∈ R^(3 x m x N) such that T(T^-1(mat)) = mat = T^-1(T(mat)). 
-    Note here that rotations here are represented in matrix form.  
+    Applys the group inverse of the SE3 transformations T = (rot,trans) ∈ SE(3)^N to N batches of m points in R3, i.e., if T = (R, t) then T^(-1) = (R^T, -R^T*t).
     """
     size_mat = size(mat)
     rotc = batched_transpose(reshape(rot, 3,3,:))
     matc = reshape(mat,3,size(mat,2),:)
     trans = reshape(trans, 3,1,:)
-    if trans != 0
-        rot_trans = batched_mul(rotc,trans)
-        rotated_mat = batched_mul(rotc,matc) .- rot_trans
-    else 
-        rotated_mat = batched_mul(rotc,matc)
-    end
+    rot_trans = batched_mul(rotc,trans)
+    rotated_mat = batched_mul(rotc,matc) .- rot_trans
+
     return reshape(rotated_mat,size_mat)
 end
 
@@ -96,10 +85,10 @@ function T_T(T_1, T_2)
     Returns the composition of two SE(3) transformations T_1 and T_2. Note that if T1 = (R1,t1), and T2 = (R2,t2) then T1*T2 = (R1*R2, R1*t2 + t1).
     T here is stored as a tuple (R,t).
     """
-
     R1, t1 = T_1
     R2, t2 = T_2
     new_rot = batched_mul(R1, R2)
     new_trans = batched_mul(R1,t2) .+ t1
+
     return (new_rot,new_trans)
 end
