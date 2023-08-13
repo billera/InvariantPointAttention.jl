a4e39e9d182ed5cc2a46c7003fb1948879d99ca2
billera <lukasbillera01@gmail.com>
2023-08-13 19:10:54 +0200

    Integrate cross and self IPA

--- src/layers.jl
+++ src/layers.jl
@@ -19,7 +19,7 @@ function (backboneupdate::BackboneUpdate)(Ti, si)
 end
 
 """
-Returns a tuple of the IPA settings, with defaults for everything except dims. This can be passed to the IPA and IPAStructureModuleLayer.
+Returns a tuple of the IPA settings, with defaults for everything except dims. This can be passed to the IPA and IPCrossAStructureModuleLayer.
 """
 IPA_settings(
     dims;
@@ -40,162 +40,6 @@ IPA_settings(
     pairwise = c_z > 0
 )
 
-"""
-Invariant Point Attention
-"""
-struct IPA
-    settings::NamedTuple
-    layers::NamedTuple
-end
-
-Flux.@functor IPA # provides parameter collection, gpu movement and more
-
-function IPA(settings::NamedTuple)
-    dims, c, N_head, N_query_points, N_point_values, c_z, Typ, pairwise = settings 
-    # Needs a slighyly unusual initialization - hat-tip: Kenta
-    init = Flux.kaiming_uniform(gain = 1.0)
-    if pairwise
-        pair = Dense(c_z => N_head, bias = false; init)
-        ipa_linear = Dense(N_head*c_z + N_head*c + 4*N_head*N_point_values => dims)
-    else
-        pair = nothing
-        ipa_linear = Dense(N_head*c + 4*N_head*N_point_values => dims)
-    end
-    layers = (
-            proj_qh = Dense(dims => c*N_head, bias = false; init),
-            proj_kh = Dense(dims => c*N_head, bias = false; init),
-            proj_vh = Dense(dims => c*N_head, bias = false; init),
-            proj_qhp = Dense(dims => 3*N_head*N_query_points, bias = false; init),
-            proj_khp = Dense(dims => 3*N_head*N_query_points, bias = false; init),
-            proj_vhp = Dense(dims => 3*N_head*N_point_values, bias = false; init),
-            ipa_linear = ipa_linear,
-            pair = pair,
-            gamma_h = ones(Typ, N_head) .* Typ(0.541)
-            )
-    
-    return IPA(settings, layers)
-end
-
-function (ipa::IPA)(Ti::Tuple{AbstractArray,AbstractArray}, si::AbstractArray; zij = nothing)
-    # Get relevant parameters from our ipa struct.
-    l = ipa.layers
-    dims, c, N_head, N_query_points, N_point_values, c_z, Typ, pairwise = ipa.settings 
-    rot_Ti, translate_Ti = Ti
-    
-    N_frames = size(si,2)
-    gamma_h = softplus(l.gamma_h)
-
-    # Constants described in the supplementary paper. 
-    w_C = Typ(sqrt(2/(9*N_query_points)))
-    w_L = Typ(sqrt(1/3))
-
-    qh = reshape(l.proj_qh(si),(c,N_head,N_frames,:))
-    kh = reshape(l.proj_kh(si),(c,N_head,N_frames,:))
-    vh = reshape(l.proj_vh(si),(c,N_head,N_frames,:))
-    qhp = reshape(l.proj_qhp(si),(3,N_head*N_query_points,N_frames,:))
-    khp = reshape(l.proj_khp(si),(3,N_head*N_query_points,N_frames,:))
-    vhp = reshape(l.proj_vhp(si),(3,N_head*N_point_values,N_frames,:))
-    
-    # Dot products between queries and keys.
-    qhT = permutedims(qh, (3, 1, 2, 4))
-    kh = permutedims(kh, (1, 3, 2, 4))
-    qhTkh = permutedims(batched_mul(qhT,kh),(3,2,1,4))
-
-    # Applying our transformations to the queries, keys, and values to put them in the global frame.
-    Tqhp = reshape(T_R3(qhp, rot_Ti,translate_Ti),3,N_head,N_query_points,N_frames,:) 
-    Tkhp = reshape(T_R3(khp, rot_Ti,translate_Ti),3,N_head,N_query_points,N_frames,:)
-    Tvhp = T_R3(vhp, rot_Ti, translate_Ti)
-
-    Tiqihp = Tqhp[:,:,:,repeat(1:N_frames, inner=N_frames),:]
-    Tjkjhp = Tkhp[:,:,:,repeat(1:N_frames, outer=N_frames),:]
-
-    norm_arg = Tiqihp .- Tjkjhp
-    norm = reshape(sqrt.(sum(norm_arg.^2,dims = 1)),N_head,N_query_points,N_frames,N_frames,:)
-    sum_norm = reshape(sum(norm, dims = 2),N_head,N_frames,N_frames,:)
-
-    att_arg = reshape(w_L*(1/Typ(sqrt(c)) .* qhTkh .- w_C/2 .* gamma_h .* sum_norm),(N_head,N_frames,N_frames, :))
-
-    if pairwise
-        bij = reshape(l.pair(zij),(N_head,N_frames,N_frames,:))
-        att = Flux.softmax(att_arg .+ w_L.*bij, dims = 3) 
-    else
-        att = Flux.softmax(att_arg, dims = 3)
-    end
-
-    # Applying the attention weights to the values.
-    broadcast_att_oh = reshape(att,(1,N_head,N_frames,N_frames,:))
-
-    #Maybe fixed a bug?
-    #broadcast_vh = reshape(vh, (c,N_head,N_frames,1,:))
-    broadcast_vh = reshape(vh, (c,N_head,1,N_frames,:))
-
-    oh = reshape(sum(broadcast_att_oh .* broadcast_vh,dims = 4), c,N_head,N_frames,:)
-
-    broadcast_att_ohp = reshape(att,(1,N_head,1,N_frames,N_frames,:))
-    broadcast_tvhp = reshape(Tvhp,(3,N_head,N_point_values,1,N_frames,:))
-
-    ohp_r = reshape(sum(broadcast_att_ohp.*broadcast_tvhp,dims=5),3,N_head*N_point_values,N_frames,:)
-    ohp = T_R3_inv(ohp_r, rot_Ti, translate_Ti)
-    normed_ohp = sqrt.(sum(ohp.^2,dims = 1))
-
-    catty = vcat(
-        reshape(oh, N_head*c, N_frames,:),
-        reshape(ohp, 3*N_head*N_point_values, N_frames,:),
-        reshape(normed_ohp, N_head*N_point_values, N_frames,:)
-        )
-    
-    if pairwise
-        broadcast_zij = reshape(zij,(c_z,1,N_frames,N_frames,:))
-        broadcast_att_zij = reshape(att,(1,N_head,N_frames,N_frames,:))
-        obh = sum(broadcast_zij .* broadcast_att_zij,dims = 4)
-        catty = vcat(catty, reshape(obh, N_head*c_z, N_frames,:))
-    end
-    
-    #Note: the skip connect happens outside of IPA itself!
-    si = l.ipa_linear(catty)
-    return si
-end
-
-"""
-Partial Structure Module - single layer - from AF2. Not a faithful repro, and doesn't include the losses etc.
-"""
-struct IPAStructureModuleLayer
-    settings::NamedTuple
-    layers::NamedTuple
-end
-Flux.@functor IPAStructureModuleLayer
-function IPAStructureModuleLayer(settings::NamedTuple; dropout_p = 0.1, af = Flux.relu)
-    dims = settings.dims
-    layers = (
-        ipa = IPA(settings),
-        ipa_norm = Chain(Dropout(dropout_p), LayerNorm(dims)),
-        trans = Chain(Dense(dims => dims, af), Dense(dims => dims, af), Dense(dims => dims)),
-        trans_norm = Chain(Dropout(dropout_p), LayerNorm(dims)),
-        backbone = BackboneUpdate(dims),
-    )
-    return IPAStructureModuleLayer(settings, layers)
-end
-function (structuremodulelayer::IPAStructureModuleLayer)(Ti, si; zij = nothing)
-    settings = structuremodulelayer.settings
-    if settings.c_z > 0 && zij === nothing
-        error("zij must be provided if c_z > 0")
-    end
-
-    l = structuremodulelayer.layers
-
-    if zij === nothing
-        si = si .+ l.ipa(Ti, si)
-    else
-        si = si .+ l.ipa(Ti, si, zij = zij)
-    end
-    si = l.ipa_norm(si)
-    si = si .+ l.trans(si)
-    si = l.trans_norm(si)
-    Ti = l.backbone(Ti, si)
-    return Ti, si
-end
-
-
 
 """
 Invariant Point Cross Attention (IPCrossA). Information flows from L (Keys, Values) to R (Queries).
@@ -204,6 +48,7 @@ struct IPCrossA
     settings::NamedTuple
     layers::NamedTuple
 end
+
 
 Flux.@functor IPCrossA # provides parameter collection, gpu movement and more
 
@@ -232,9 +77,36 @@ function IPCrossA(settings::NamedTuple)
     
     return IPCrossA(settings, layers)
 end
+
+
+
+"""
+Strictly Self-IPA initialization
+"""
+# We could skip making this struct and just have it be a cross IPA struct. 
+struct IPA
+    settings::NamedTuple
+    layers::NamedTuple
+end
+
+Flux.@functor IPA
+
+function IPA(settings::NamedTuple)
+    crossL = IPCrossA(settings)
+    return IPA(crossL.settings, crossL.layers)
+end
+
+"""
+Self-IPA can be run from both IPA and cross IPA, allowing for flexibility. Simply calls cross IPA on itself. 
+"""
+function (ipa::Union{IPA, IPCrossA})(T::Tuple{AbstractArray,AbstractArray}, S::AbstractArray; Z = nothing)
+    return ipa(T, S, T, S; zij = Z)
+end
+
+
 #Attention props from L (Keys, Values) to R (Queries).
 #Because IPA uses Q'K, our pairwise matrices are R-by-L
-function (ipa::IPCrossA)(TiL::Tuple{AbstractArray,AbstractArray}, siL::AbstractArray, TiR::Tuple{AbstractArray,AbstractArray}, siR::AbstractArray; zij = nothing)
+function (ipa::Union{IPCrossA, IPA})(TiL::Tuple{AbstractArray,AbstractArray}, siL::AbstractArray, TiR::Tuple{AbstractArray,AbstractArray}, siR::AbstractArray; zij = nothing)
     if zij != nothing
         #This is assuming the dims of zij are c, N_frames_L, N_frames_R, batch
         @assert size(zij,2) == size(siR,2)
@@ -256,6 +128,7 @@ function (ipa::IPCrossA)(TiL::Tuple{AbstractArray,AbstractArray}, siL::AbstractA
     # Constants described in the supplementary paper. 
     w_C = Typ(sqrt(2/(9*N_query_points)))
     w_L = Typ(sqrt(1/3))
+    scale = Typ(1/sqrt(c))
 
     qh = reshape(l.proj_qh(siR),(c,N_head,N_frames_R,:))
     kh = reshape(l.proj_kh(siL),(c,N_head,N_frames_L,:))
@@ -311,7 +184,7 @@ function (ipa::IPCrossA)(TiL::Tuple{AbstractArray,AbstractArray}, siL::AbstractA
     sum_norm = reshape(sum(norm, dims = 2),N_head,N_frames_R,N_frames_L,:) #Sum over points for each head
     #@show size(sum_norm)
     
-    att_arg = reshape(w_L*(1/Typ(sqrt(c)) .* qhTkh .- w_C/2 .* gamma_h .* sum_norm),(N_head,N_frames_R,N_frames_L, :))
+    att_arg = reshape(w_L*(scale .* qhTkh .- w_C/2 .* gamma_h .* sum_norm),(N_head,N_frames_R,N_frames_L, :))
     #@show size(att_arg)
     
     if pairwise
@@ -367,17 +240,59 @@ function (ipa::IPCrossA)(TiL::Tuple{AbstractArray,AbstractArray}, siL::AbstractA
     return si 
 end
 
-#= Testing
-len_L, len_R, dim = 4,5,8
-T_L = (get_rotation(len_L), randn(3,len_L))
-T_R = (get_rotation(len_R), randn(3,len_R))
-S_L = randn(dim,len_L)
-S_R = randn(dim,len_R)
-T_L, S_L, T_R, S_R = (T_L, S_L, T_R, S_R) |> gpu
-ipca = IPCrossA(IPA_settings(dim, N_query_points = 7)) |> gpu
-ipca(T_L, S_L, T_R, S_R)
-
-loss, grads = Flux.withgradient(params) do
-    mean(ipca(T_L, S_L, T_R, S_R) .^ 2)
+"""
+Cross IPA Partial Structure Module initialization - single layer - adapted from AF2. From left to right. 
+"""
+struct IPCrossAStructureModuleLayer
+    settings::NamedTuple
+    layers::NamedTuple
 end
-=#
+Flux.@functor IPCrossAStructureModuleLayer
+function IPCrossAStructureModuleLayer(settings::NamedTuple; dropout_p = 0.1, af = Flux.relu)
+    dims = settings.dims
+    layers = (
+        ipa = IPCrossA(settings),
+        ipa_norm = Chain(Dropout(dropout_p), LayerNorm(dims)),
+        trans = Chain(Dense(dims => dims, af), Dense(dims => dims, af), Dense(dims => dims)),
+        trans_norm = Chain(Dropout(dropout_p), LayerNorm(dims)),
+        backbone = BackboneUpdate(dims),
+    )
+    return IPCrossAStructureModuleLayer(settings, layers)
+end
+
+"""
+Self IPA Partial Structure Module initialization - single layer - adapted from AF2. 
+"""
+
+# We could skip making this struct and just have it be a cross IPA struct. 
+struct IPAStructureModuleLayer
+    settings::NamedTuple
+    layers::NamedTuple
+end
+Flux.@functor IPAStructureModuleLayer
+
+function IPAStructureModuleLayer(settings::NamedTuple)
+    crossL = IPCrossAStructureModuleLayer(settings)
+    return IPAStructureModuleLayer(crossL.settings, crossL.layers)
+end
+function (structuremodulelayer::Union{IPAStructureModuleLayer, IPCrossAStructureModuleLayer} )(T, S; zij = nothing)
+    return structuremodulelayer(T, S, T, S; zij = zij)
+end
+
+"""
+Cross IPA Partial Structure Module - single layer - adapted from AF2. From left to right. 
+"""
+function (structuremodulelayer::Union{IPCrossAStructureModuleLayer, IPAStructureModuleLayer})(T_L, S_L, T_R, S_R; zij = nothing)
+    settings = structuremodulelayer.settings
+    if settings.c_z > 0 && zij === nothing
+        error("zij must be provided if c_z > 0")
+    end
+
+    l = structuremodulelayer.layers
+    S_R = S_R .+ l.ipa(T_L, S_L,T_R,S_R, zij = zij)
+    S_R = l.ipa_norm(S_R)
+    S_R = S_R .+ l.trans(S_R)
+    S_R = l.trans_norm(S_R)
+    T_R = l.backbone(T_R, S_R)
+    return T_R, S_R
+end
