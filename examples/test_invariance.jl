462a5c19a25d14064761fc34ccc4fdd9598b6b35
billera <lukasbillera01@gmail.com>
2023-08-13 19:12:36 +0200

    Update invariance test

--- examples/test_invariance.jl
+++ examples/test_invariance.jl
@@ -1,4 +1,10 @@
-include("../src/attention.jl")
+using LinearAlgebra
+using StatsBase
+using Flux
+using CUDA
+
+include("../src/layers.jl")
+include("../src/rotational_utils.jl")
 
 batch_size = 32
 frames = 64
@@ -11,16 +17,14 @@ T_loc = (get_rotation(frames,batch_size), get_translation(frames,batch_size)) |>
 # Get 1 global SE(3) transformation for each batch.
 T_glob = (get_rotation(batch_size), get_translation(batch_size)) 
 
-# Replicating the global SE(3) transformation for each frame so it can be applied to each frame's local SE(3) transformation.
 T_glob = (stack([T_glob[1] for i in 1:frames],dims = 3), stack([T_glob[2] for i in 1:frames],dims = 3)) |> gpu
 T_new = T_T(T_glob,T_loc)
 
-# Define IPA block. 
-ipa = IPA(dim, 16, 12, 4, 8) |> gpu
+ipa = IPAStructureModuleLayer(IPA_settings(dim)) |> gpu
 
-si_loc, tmp = ipa(si, T_loc)
-si_glob, tmp = ipa(si, T_new)
+T_loc, si_loc = ipa(T_loc,si)
+T_glob, si_glob = ipa(T_new, si)
 
 invariance_error = maximum(abs.(si_glob .- si_loc)) 
 
-@show invariance_error # ≈ 2f-6
+@show invariance_error # ≈ 10^-6
